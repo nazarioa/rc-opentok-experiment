f5dd0e74e75195d2ba611885ac6f6cce8ceffc5d
Nazario A. Ayala <nazario.ayala@rallyhealth.com>
2020-08-27 16:45:17 -0700

    leveraged connection

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
-import { OpentokService } from './opentok.service';
+import {OpentokService} from './opentok.service';
+import {filter, switchMap, switchMapTo, tap} from "rxjs/operators";
 import {OtEventNames} from "./opentok.model";
 
 const otService = new OpentokService();
 
 
-const apiKey  = '';
+const apiKey = '';
 const sessionId = '';
 const token = '';
 
 const initializedTokBoxSession = otService.initSession(apiKey, sessionId);
 
-initializedTokBoxSession.pipe().subscribe(() => console.log('Guess what?!'));
+initializedTokBoxSession.pipe(
+  switchMapTo(otService.connectSession(token)),
+).subscribe({
+  next: n => console.log('Success initLiveSession', n),
+  error: e => console.log('Fail initLiveSession', e),
+});
+
+otService.coachStreamLifecycleEvents$.pipe(
+  filter(event => event.type === OtEventNames.StreamCreated),
+  tap(_ => console.log('>>> am I undefined', _)),
+  switchMap(event => otService.sessionMediaSubscribe('camera-outlet', event['stream'], {
+    fitMode: 'contain',
+    height: '100%',
+    insertMode: 'append',
+    showControls: false,
+    subscribeToAudio: false,
+    subscribeToVideo: true,
+    width: '100%',
+  }))
+).subscribe({
+  next: n => console.log('Success', n),
+  error: e => console.log('Fail', e),
+});
