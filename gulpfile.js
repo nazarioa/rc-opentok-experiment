9b99e9d750b30b9876285cd174a1c30e966bcc9e
Nazario A. Ayala <nazario.ayala@rallyhealth.com>
2020-09-01 11:07:42 -0700

    Added favicon to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
-var sourcemaps = require("gulp-sourcemaps");
-var buffer = require("vinyl-buffer");
-var paths = {
-  pages: ["src/*.html"],
+const gulp = require("gulp");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
+const sourcemaps = require("gulp-sourcemaps");
+const buffer = require("vinyl-buffer");
+const paths = {
+    pages: ["src/*.html"],
+    favicon: ["src/favicon.ico"],
 };
 
-gulp.task("copy-html", function () {
-  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
-});
+gulp.task("copy-html", () => gulp.src(paths.pages).pipe(gulp.dest("dist")));
+gulp.task("copy-assets", () => gulp.src(paths.favicon).pipe(gulp.dest("dist")));
 
 gulp.task(
-  "default",
-  gulp.series(gulp.parallel("copy-html"), function () {
-    return browserify({
-      basedir: ".",
-      debug: true,
-      entries: ["src/index.ts"],
-      cache: {},
-      packageCache: {},
-    })
-      .plugin(tsify)
-      .transform("babelify", {
-        presets: ["es2015"],
-        extensions: [".ts"],
-      })
-      .bundle()
-      .pipe(source("bundle.js"))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write("./"))
-      .pipe(gulp.dest("dist"));
-  })
+    "default",
+    gulp.series(
+        gulp.parallel("copy-html", "copy-assets"), () =>
+            browserify({
+                basedir: ".",
+                debug: true,
+                entries: ["src/index.ts"],
+                cache: {},
+                packageCache: {},
+            })
+                .plugin(tsify)
+                .transform("babelify", {
+                    presets: ["es2015"],
+                    extensions: [".ts"],
+                })
+                .bundle()
+                .pipe(source("bundle.js"))
+                .pipe(buffer())
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(sourcemaps.write("./"))
+                .pipe(gulp.dest("dist"))
+    )
 );
